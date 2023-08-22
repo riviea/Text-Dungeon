8ec0bea579bda2f36d4a3ca2194a64393377ba53
Lee dohyun <riviea@naver.com>
2023-08-22 23:42:09 +0900

    Add EquipItem, DataManager

---
+++ TextDungeon/Data/DataManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextDungeon
+{
+    public class DataManager
+    {
+        private static DataManager instance = new DataManager();
+
+        // member
+        private List<Item> items = new List<Item>();
+        private Player player = new Player();
+
+        // property
+        public Player Player { get { return player; } }
+        public List<Item> Items { get { return items; } }
+
+        public static DataManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    DataManager temp = new DataManager();
+                    instance = temp;
+                }
+                return instance;
+            }
+            private set { instance = value; }
+        }
+
+        public DataManager()
+        {
+            instance = this;
+        }
+
+        public void LoadItems()
+        {
+            // ...
+        } 
+    }
+}
---
+++ TextDungeon/Data/EquipItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextDungeon
+{
+    public class EquipItem : Item
+    {
+        //parts
+        public enum Parts { Head, Body, Legs, Shoose, Arm, Weapon }
+        private Parts part;
+
+        // Increase Status
+        public EquipItem()
+        {
+
+        }
+
+        public EquipItem(string name, int atk, int def, int hp, string desc, Parts _part)
+        {
+            this.name = name;
+            this.atk = atk;
+            this.def = def;
+            this.hp = hp;
+            this.desc = desc;
+            this.part = _part;
+        }
+    }
+}
